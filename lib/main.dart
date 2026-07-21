f9cb3290cc1a3e01547e8de29f865f659367911a
Raafat <raafatm2001@gmail.com>
2026-07-21 04:20:46 +0300

    Initial App

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TestRun());
+}
+
+class TestRun extends StatelessWidget {
+  const TestRun({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
